7b2cfdc7948c309f7388f47711cdb09e51f11c95
Pranay Umredkar <71922973+Pranum@users.noreply.github.com>
2022-01-06 10:13:25 +0530

    Update SQL-Covid19DataExploration.sql

--- SQL-Covid19DataExploration.sql
+++ SQL-Covid19DataExploration.sql
@@ -2,6 +2,7 @@
 Covid 19 Data Exploration 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 */
+-- Dataset link is in readme file.
 
 
 Select *
